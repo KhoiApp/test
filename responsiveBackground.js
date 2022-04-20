4c6a4e1837cd37c293f20ebacb9bde265a448b58
khoile-applitools <84586972+khoile-applitools@users.noreply.github.com>
2022-04-20 09:29:46 -0700

    Update responsiveBackground.js

--- responsiveBackground.js
+++ responsiveBackground.js
@@ -1,14 +1,15 @@
-var para = document.querySelector('article');
+//var para = document.querySelector('article');
+var mql = window.matchMedia('(max-width: 600px)');
 
 function screenTest(e) {
   if (e.matches) {
     /* the viewport is 600 pixels wide or less */
     //para.textContent = 'This is a narrow screen — less than 600px wide.';
-    para.style.backgroundColor = 'red';
+    document.body.style.backgroundColor = 'red';
   } else {
     /* the viewport is more than 600 pixels wide */
     //para.textContent = 'This is a wide screen — more than 600px wide.';
-    para.backgroundColor = 'blue';
+    document.body.style.backgroundColor = 'blue';
   }
 }
 
